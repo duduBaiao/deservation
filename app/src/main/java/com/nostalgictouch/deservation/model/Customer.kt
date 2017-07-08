a1d7f1977d02cdb5505393909fc6c0fea4e1e511
Eduardo Baiao <eduardobaiao@gmail.com>
2017-07-08 20:47:00 -0300

    The Customer model now is a Parcelable

--- app/src/main/java/com/nostalgictouch/deservation/model/Customer.kt
+++ app/src/main/java/com/nostalgictouch/deservation/model/Customer.kt
@@ -1,11 +1,35 @@
 package com.nostalgictouch.deservation.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Customer(
         val id: Int,
         val firstName: String,
-        val lastName: String) {
+        val lastName: String) : Parcelable {
 
-    fun  fullName(): String {
+    fun fullName(): String {
         return firstName + " " + lastName;
+    }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Customer> = object : Parcelable.Creator<Customer> {
+            override fun createFromParcel(source: Parcel): Customer = Customer(source)
+            override fun newArray(size: Int): Array<Customer?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel) : this(
+            source.readInt(),
+            source.readString(),
+            source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(id)
+        dest.writeString(firstName)
+        dest.writeString(lastName)
     }
 }
