98d2d6b0ef4204cd189d4c90903eb0be3de1b858
Eduardo Baiao <eduardobaiao@gmail.com>
2017-07-09 13:13:10 -0300

    Custom matcher added to find a view holder by its position

--- app/src/androidTest/java/com/nostalgictouch/deservation/matchers/CustomMatchers.kt
+++ app/src/androidTest/java/com/nostalgictouch/deservation/matchers/CustomMatchers.kt
@@ -1,15 +1,19 @@
 package com.nostalgictouch.deservation.matchers
 
+import android.support.annotation.NonNull
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.BoundedMatcher
 import android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.view.View
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.`is`
+
 
 object CustomMatchers {
 
@@ -32,6 +36,23 @@ object CustomMatchers {
             override fun describeTo(description: Description) {
                 description.appendText("with toolbar title: ")
                 textMatcher.describeTo(description)
+            }
+        }
+    }
+
+    fun atPosition(position: Int, @NonNull itemMatcher: Matcher<View>): Matcher<View> {
+        checkNotNull(itemMatcher)
+
+        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position) ?: return false
+                return itemMatcher.matches(viewHolder.itemView)
             }
         }
     }
